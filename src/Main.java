ffa6576990f3910260116bb700c3f38ae7d3e37b
Tonfiskmedris <40519864+Tonfiskmedris@users.noreply.github.com>
2021-04-02 00:07:08 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,28 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int row;
-        int columns;
 
-        System.out.println("Specify number of rows: ");
-        row = input.nextInt();
-        System.out.println("Specify number of columns: ");
-        columns = input.nextInt();
+        Scanner keyBoardInput = new Scanner(System.in);
 
-        for (int i=1;i<=row;i++){
-            for (int j = 1; j<=columns;j++){
-                System.out.print("X");
-            }
-            System.out.println();
+        String answerOne, answerTwo, answerFour;
+        int answerThree;
 
+        while (true) {
+
+            System.out.println("Is the person a\na)male b)female");
+            answerOne = keyBoardInput.nextLine();
+            System.out.println("Are you already a customer:\na)yes b)no");
+            answerTwo = keyBoardInput.nextLine();
+            System.out.println("How old are you?\n1)0-10 2)11-20 3)20+");
+            answerThree = Integer.parseInt(keyBoardInput.nextLine());
+            System.out.println("Have you answered everything correctly?\ny/n");
+            answerFour = keyBoardInput.nextLine();
+
+            if (answerFour.equals("y"))
+                break;
         }
-
-
-
+        System.out.printf("First answer: %s%nSecond Answer: %s%nThird Answer: %s%nForth Answer: %s%n", answerOne, answerTwo, answerThree, answerFour);
     }
 }
